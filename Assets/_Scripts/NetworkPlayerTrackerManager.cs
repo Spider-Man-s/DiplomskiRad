8b2dcb4d8e520cc111143ef820dade46e385602c
verticals.iot.lab <verticals.iot.lab@gmail.com>
2026-03-06 14:47:40 +0100

    simulation works
    2 players in simulations can see each other and see transforms

--- Assets/_Scripts/NetworkPlayerTrackerManager.cs
+++ Assets/_Scripts/NetworkPlayerTrackerManager.cs
@@ -1,3 +1,156 @@
+using Fusion;
+using UnityEngine;
+
+public class NetworkPlayerTrackerManager : NetworkBehaviour
+{
+    [Header("Network Visual Targets")]
+    [SerializeField] private Transform netHead;
+    [SerializeField] private Transform netLeftHand;
+    [SerializeField] private Transform netRightHand;
+
+    [Header("Rigs")]
+    [SerializeField] private GameObject metaRig;
+    [SerializeField] private GameObject xrealRig;
+
+    [Header("Meta Tracked Transforms")]
+    [SerializeField] private Transform metaHead;
+    [SerializeField] private Transform metaLeftHand;
+    [SerializeField] private Transform metaRightHand;
+
+    [Header("Xreal Tracked Transforms")]
+    [SerializeField] private Transform xrealHead;
+    [SerializeField] private Transform xrealLeftHand;
+    [SerializeField] private Transform xrealRightHand;
+
+    private Transform srcHead;
+    private Transform srcLeft;
+    private Transform srcRight;
+
+    // Host/state-authority-owned replicated pose
+    [Networked] private Vector3 HeadPos { get; set; }
+    [Networked] private Quaternion HeadRot { get; set; }
+
+    [Networked] private Vector3 LeftPos { get; set; }
+    [Networked] private Quaternion LeftRot { get; set; }
+
+    [Networked] private Vector3 RightPos { get; set; }
+    [Networked] private Quaternion RightRot { get; set; }
+
+    public override void Spawned()
+    {
+        if (!Object.HasInputAuthority)
+        {
+            metaRig.SetActive(false);
+            xrealRig.SetActive(false);
+            return;
+        }
+
+#if META_BUILD
+        ActivateMeta();
+#elif XREAL_BUILD
+        ActivateXreal();
+#else
+        metaRig.SetActive(false);
+        xrealRig.SetActive(false);
+        Debug.LogError("No build symbol defined. Define META_BUILD or XREAL_BUILD.");
+#endif
+    }
+
+    private void ActivateMeta()
+    {
+        metaRig.SetActive(true);
+        xrealRig.SetActive(false);
+
+        srcHead = metaHead;
+        srcLeft = metaLeftHand;
+        srcRight = metaRightHand;
+    }
+
+    private void ActivateXreal()
+    {
+        metaRig.SetActive(false);
+        xrealRig.SetActive(true);
+
+        srcHead = xrealHead;
+        srcLeft = xrealLeftHand;
+        srcRight = xrealRightHand;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        // Local owner samples XR tracking and sends to host
+        if (Object.HasInputAuthority)
+        {
+            if (srcHead && srcLeft && srcRight)
+            {
+                RPC_SendPose(
+                    srcHead.position, srcHead.rotation,
+                    srcLeft.position, srcLeft.rotation,
+                    srcRight.position, srcRight.rotation
+                );
+            }
+        }
+
+        // State authority can also set its own pose directly without RPC bounce
+        if (Object.HasStateAuthority && Object.HasInputAuthority)
+        {
+            if (srcHead && srcLeft && srcRight)
+            {
+                HeadPos = srcHead.position;
+                HeadRot = srcHead.rotation;
+
+                LeftPos = srcLeft.position;
+                LeftRot = srcLeft.rotation;
+
+                RightPos = srcRight.position;
+                RightRot = srcRight.rotation;
+            }
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority, Channel = RpcChannel.Unreliable)]
+    private void RPC_SendPose(
+        Vector3 headPos, Quaternion headRot,
+        Vector3 leftPos, Quaternion leftRot,
+        Vector3 rightPos, Quaternion rightRot)
+    {
+        HeadPos = headPos;
+        HeadRot = headRot;
+
+        LeftPos = leftPos;
+        LeftRot = leftRot;
+
+        RightPos = rightPos;
+        RightRot = rightRot;
+    }
+
+    public override void Render()
+    {
+        // Everyone renders the replicated pose from the host-owned networked state
+        if (netHead)
+        {
+            netHead.position = HeadPos;
+            netHead.rotation = HeadRot;
+        }
+
+        if (netLeftHand)
+        {
+            netLeftHand.position = LeftPos;
+            netLeftHand.rotation = LeftRot;
+        }
+
+        if (netRightHand)
+        {
+            netRightHand.position = RightPos;
+            netRightHand.rotation = RightRot;
+        }
+    }
+}
+
+
+
+/*
+
 using UnityEngine;
 using Fusion;
 
@@ -84,3 +237,5 @@ public class NetworkPlayerTrackerManager : NetworkBehaviour
         netRightHand.rotation = srcRight.rotation;
     }
 }
+
+*/
